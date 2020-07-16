a0eb52a3181c127e396289e9912fd19a27154d31
y.hoshi <ysk.hoshi.1118@gmail.com>
2020-07-17 01:55:20 +0900

    参照追加

--- src/server.js
+++ src/server.js
@@ -3,23 +3,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const personController = require('./controllers/personController');
+
 const PORT = 3000;
 
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get('/', personController.getAllPersons, (req, res) => {
+    console.log('get request on root');
+    res.send(req.data);
 });
 
 app.listen(PORT);
 console.log(`Running on http://${PORT}`);
 
-mongoose.connect(
-    'mongodb://mongo-db:27017',
-    {
-        useUnifiedTopology: true,
-        useNewUrlParser: true
-    }
-).catch(e => console.log(e));
+mongoose.set('useCreateIndex', true)
+    .connect(
+        'mongodb://mongo-db:27017',
+        {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        }
+    ).catch(e => console.log(e));
 
 const db = mongoose.connection;
 db.once('open', () => {
